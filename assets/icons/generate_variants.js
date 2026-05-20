7ca2eda1421ab9dda415262e229ef9e8e3aa1c1b
Spryra <spryra@github.com>
2026-05-20 17:28:48 +0300

    feat: create all 120 admin & status icon variants (10 icons x 12 variants)

--- assets/icons/generate_variants.js
+++ assets/icons/generate_variants.js
@@ -113,11 +113,12 @@ function generateVariants() {
   const deliveryDir = path.join(iconDir, 'delivery')
   const productDir = path.join(iconDir, 'product')
   const navigationDir = path.join(iconDir, 'navigation')
+  const adminDir = path.join(iconDir, 'admin')
 
   const baseIcons = []
 
   // Find base icons (24px versions without size/state suffixes)
-  for (const dir of [paymentDir, deliveryDir, productDir, navigationDir]) {
+  for (const dir of [paymentDir, deliveryDir, productDir, navigationDir, adminDir]) {
     if (!fs.existsSync(dir)) {
       console.warn(`Warning: Directory ${dir} does not exist`)
       continue
